f0f6c5128c075b4d3639a5a2d28ed35479a5f83f
MSTC Signal Processing and Machine Learning for Big Data <master.stc.ssr@gmail.com>
2017-09-21 19:17:14 +0200

    Update Working.py

--- Working.py
+++ Working.py
@@ -1,16 +1,6 @@
 
 
-import urllib
-import random
+## Python script for MSTC classroom activities
 
-# Load the data from remote location (URL)
-file = urllib.request.urlopen(
-    "https://gist.githubusercontent.com/twielfaert/a0972bf366d9aaf6cb1206c16bf93731/raw/dde46ad1fa41f442971726f34ad03aaac85f5414/Donna-Tartt-The-Goldfinch.csv")
-f = file.read()
-
-# Transform the bitstream into strings
-text = f.decode(encoding='utf-8', errors='ignore')
-
-lines = text.split("\n")
 
 
